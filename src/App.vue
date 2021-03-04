e5fb9a3ef70bbf244038208df9e02f24936c3653
Martín <soncinimartin@gmail.com>
2021-03-04 12:32:56 -0300

    va tomando forma eh

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Rustic Music</h1>
+    <section>
+      <nav>
+        <div>
+          <input
+            type="text"
+            placeholder="Buscar canción o artista"
+            v-model="searchQuery"
+          />
+        </div>
+        <div>
+          <button @click="searchTrack">Buscar</button>
+          <button @click="cancelDeleteSearch">x</button>
+          <p v-if="coincidenceTrack">{{ searchMessage }}</p>
+        </div>
+      </nav>
+      <div>
+        <table v-if="tracksFiltered.length > 0">
+          <tr>
+            <th>Artista</th>
+            <th>Canción</th>
+          </tr>
+          <tr v-for="track in tracksFiltered" :key="track.name">
+            <td>{{ track.artist }}</td>
+            <td>{{ track.name }}</td>
+          </tr>
+        </table>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+const tracks = [
+  { name: "muchacha", artist: "spinetta" },
+  { name: "aca en el baile", artist: "el pepo" },
+  { name: "i was made for loving you", artist: "kiss" }
+];
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      searchQuery: "",
+      tracks: []
+    };
+  },
+  computed: {
+    searchMessage() {
+      return `${this.tracksFiltered.length} ${
+        this.tracksFiltered.length === 1
+          ? "resultado encontrado"
+          : "resultados encontrados"
+      }`;
+    },
+    tracksFiltered() {
+      if (this.searchQuery === "") {
+        return false;
+      }
+      return this.tracks.filter(
+        track =>
+          track.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+          track.artist.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    },
+    coincidenceTrack() {
+      return this.searchQuery != "" && this.tracks.length > 0 ? true : false;
+    }
+  },
+  methods: {
+    searchTrack() {
+      this.tracks = tracks;
+    },
+    cancelDeleteSearch() {
+      this.tracks = [];
+      this.searchQuery = "";
+    }
   }
 };
 </script>
@@ -22,7 +86,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #332f29;
   margin-top: 60px;
+}
+table {
+  margin: auto;
 }
 </style>
