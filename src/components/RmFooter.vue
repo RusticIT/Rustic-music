b5eae143a1dfa1a6df45d0d6ef4ddbc487d7033e
Martín <soncinimartin@gmail.com>
2021-03-19 22:29:11 -0300

    update favicon add deezer logo

--- src/components/RmFooter.vue
+++ src/components/RmFooter.vue
@@ -1,8 +1,14 @@
 <template>
-  <footer class="bg-gray-900 text-white p-6 fixed w-screen bottom-0">
+  <footer
+    class="bg-gray-900 text-white p-6 fixed flex justify-between w-screen bottom-0"
+  >
     <div class="flex items-end">
       <img class="h-10" src="../assets/rusticLogo.svg" alt="" />
       <span class="mx-2 ">2021</span>
+    </div>
+    <div class="flex">
+      <span>Powered by</span>
+      <img class="h-10" src="../assets/deezerLogo.svg" alt="" />
     </div>
   </footer>
 </template>
